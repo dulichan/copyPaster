fe781e7d7698eb41a0309287ca957b5626886847
Chan <dulithaz@gmail.com>
2014-03-02 09:49:41 +0530

    Added a method to copy files based on parameters

--- client.rb
+++ client.rb
@@ -2,4 +2,5 @@ $LOAD_PATH << '.'
 require "file-copy"
 require 'benchmark'
 
-puts Benchmark.measure { FileCopy.say "III" }
+# puts Benchmark.measure { FileCopy.say "III" }
+puts Benchmark.measure { FileCopy.copy("/Volumes/Battlestation/Ano_Hana/", "/Volumes/chan_drive/Development/scratch/to/", "N") }
--- file-copy.rb
+++ file-copy.rb
@@ -6,9 +6,9 @@ module FileCopy
 	def self.say(m)
 		group = Workers::TaskGroup.new
 		#FileUtils.copy_entry "from", "to"
-		Dir.foreach("/Volumes/chan_drive/Development/scratch/from/") do |entry|		   
-			in_name     = "/Volumes/chan_drive/Development/scratch/from/"+entry
-			out_name    = "/Volumes/Battlestation/Development/scratch/to/"+entry
+		Dir.foreach("/Volumes/Battlestation/Ano_Hana") do |entry|		   
+			in_name     = "/Volumes/Battlestation/Ano_Hana/"+entry
+			out_name    = "/Volumes/chan_drive/Development/scratch/to/"+entry
 			if File.file?(in_name)
 				in_file     = File.new(in_name, "r")
 				group.add do
@@ -17,7 +17,6 @@ module FileCopy
 					in_size     = File.size(in_name)
 					batch_bytes = ( in_size / 100 ).ceil
 					total       = 0
-
 					buffer      = in_file.sysread(batch_bytes)
 					while total < in_size do
 						out_file.syswrite(buffer)
@@ -33,5 +32,42 @@ module FileCopy
 			end
 		end
 		group.run
+	end
+	def self.rcopy(input, output)
+
+	end
+	def self.ncopy(input, output)
+		if File.file?(input)
+			in_file     = File.new(input, "r")
+	    	out_file    = File.new(output, "w")
+			p_bar       = ProgressBar.new('Copying file:-'+output, 100)
+			in_size     = File.size(input)
+			batch_bytes = ( in_size / 100 ).ceil
+			total       = 0
+			buffer      = in_file.sysread(batch_bytes)
+			while total < in_size do
+				out_file.syswrite(buffer)
+				total += batch_bytes
+				p_bar.inc
+				if (in_size - total) < batch_bytes
+			  		batch_bytes = (in_size - total)
+				end
+				buffer = in_file.sysread(batch_bytes)
+			end
+			p_bar.finish
+		end
+	end
+	def self.copy(input, output, type)
+		if type=='P'
+			group = Workers::TaskGroup.new
+
+			group.run
+		else 
+			Dir.foreach(input) do |entry|		   
+				in_name     = input+entry
+				out_name    = output+entry
+				ncopy(in_name, out_name)
+			end
+		end
 	end
 end
